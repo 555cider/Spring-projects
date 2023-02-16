5634663592d9e1c6da2c37ec9d64dff0b5d8be3d
Mooyong Lee <sosnsg@gmail.com>
2023-02-16 21:49:09 +0900

    feat: disable CSRF protection

---
+++ gateway/src/main/java/com/piano/gateway/config/WebFluxSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.piano.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class WebFluxSecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+	return http.csrf().disable().build();
+    }
+
+}
