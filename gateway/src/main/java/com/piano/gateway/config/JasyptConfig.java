fbf061701a205f9f9f1926d4cf2080f5a384cd50
Mooyong Lee <sosnsg@gmail.com>
2023-02-18 22:45:05 +0900

    feat: encrypt properties with AES 256-GCM by Jasypt

---
+++ gateway/src/main/java/com/piano/gateway/config/JasyptConfig.java
@@ -0,0 +1,23 @@
+package com.piano.gateway.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.ulisesbocchio.jasyptspringboot.encryptor.SimpleGCMConfig;
+import com.ulisesbocchio.jasyptspringboot.encryptor.SimpleGCMStringEncryptor;
+
+@Configuration
+public class JasyptConfig {
+
+    @Bean(name = "jasyptStringEncryptor")
+    StringEncryptor stringEncryptor() {
+	SimpleGCMConfig config = new SimpleGCMConfig();
+	// add "secret_key.b64" to ".gitginore" in production
+	config.setSecretKeyLocation("classpath:secret_key.b64");
+	// add "-Djasypt.encryptor.password={password}" to VM options in production
+	config.setSecretKeyPassword("password");
+	return new SimpleGCMStringEncryptor(config);
+    }
+
+}
